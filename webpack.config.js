337e9e6495b9593f50a8c9965888acb86ae882e5
QusaiAlbonni <axinkiro@gmail.com>
2024-06-20 15:49:50 +0300

    webpack configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,17 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const isPersonal = process.env.PERSONAL === 'true';
+
 
 module.exports = {
     mode: "development",
-    entry: './src/index.js',
+    entry: {
+        main: isPersonal ? './src/personal.js' : './src/index.js',
+        wind: './src/wind.js'
+    },
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true
     },
