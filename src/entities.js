7aef2e08027b5c2e188d2e2c38e9f56fa9de957a
QusaiAlbonni <axinkiro@gmail.com>
2024-06-21 16:16:15 +0300

    fixes

--- src/entities.js
+++ src/entities.js
@@ -1,43 +1,43 @@
 import * as THREE from "three";
 import {
-  EffectComposer,
-  BloomEffect,
-  EffectPass,
-  FXAAEffect,
-  SSAOEffect,
+    EffectComposer,
+    BloomEffect,
+    EffectPass,
+    FXAAEffect,
+    SSAOEffect,
 } from "postprocessing";
 import { Sky } from "three/addons/objects/Sky.js";
 import Water from "./water";
 import {
-  WaterBehavior,
-  CameraBehavior,
-  NewScript,
-  SkyBehavior,
-  BoxBehavior,
+    WaterBehavior,
+    CameraBehavior,
+    NewScript,
+    SkyBehavior,
+    BoxBehavior,
 } from "./scripts/behaviors";
 
 const renderer = new THREE.WebGLRenderer({
-  powerPreference: "high-performance",
+    powerPreference: "high-performance",
 });
 const mainScene = new THREE.Scene();
 const composer = new EffectComposer(renderer);
 const mainCamera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
 );
 
 const gameRenderEntity = {
-  tags: ["MainGameRender"],
-  c: {
-    gameRender: {
-      type: "GameRender",
-      renderer: renderer,
-      scene: mainScene,
-      composer: composer,
+    tags: ["MainGameRender"],
+    c: {
+        gameRender: {
+            type: "GameRender",
+            renderer: renderer,
+            scene: mainScene,
+            composer: composer,
+        },
     },
-  },
 };
 
 
@@ -92,74 +92,75 @@ const bloomPass = new EffectPass(mainCamera, bloomEffect);
 const fxaaPass = new EffectPass(mainCamera, new FXAAEffect());
 
 const postProcessingEntity = {
-  c: {
-    bloom: {
-      type: "PassComponent",
-      pass: bloomPass,
-      composer: composer,
+    c: {
+        bloom: {
+            type: "PassComponent",
+            pass: bloomPass,
+            composer: composer,
+        },
+        fxaa: {
+            type: "PassComponent",
+            pass: fxaaPass,
+            composer: composer,
+        },
+        ssao: {
+            type: "PassComponent",
+            pass: new EffectPass(mainCamera, new SSAOEffect(mainCamera, mainScene)),
+        },
     },
-    fxaa: {
-      type: "PassComponent",
-      pass: fxaaPass,
-      composer: composer,
-    },
-    ssao: {
-      type: "PassComponent",
-      pass: new EffectPass(mainCamera, new SSAOEffect(mainCamera, mainScene)),
-    },
-  },
 };
 
 const sky = new Sky();
 
 const skyEntity = {
-  c: {
-    meshFilter: {
-      type: "MeshFilter",
-      mesh: sky,
-      scene: mainScene,
+    c: {
+        meshFilter: {
+            type: "MeshFilter",
+            mesh: sky,
+            scene: mainScene,
+        },
+        transform: {
+            type: "Transform",
+            obj: sky,
+        },
+        script: {
+            type: "Script",
+            script: new SkyBehavior(),
+        },
     },
-    transform: {
-      type: "Transform",
-      obj: sky,
-    },
-    script: {
-      type: "Script",
-      script: new SkyBehavior(),
-    },
-  },
 };
 
 
 
 const me = new THREE.Mesh(nonIndexGeo, mat);
 const exampleBoxEntity = {
-  c: {
-    meshFilter: {
-      type: "MeshFilter",
-      mesh: me,
-      scene: mainScene,
-    },
-    script: {
-      type: "Script",
-      script: new NewScript(),
-    },
-    transform: {
-      type: "Transform",
-      obj: me,
-    },
-    gui: {
-      type: "GUIcomponent",
-      list: [
-        {
-          path: [""],
-          guiType: "slider",
-          properityName: "x",
-          target: me.position,
-          max: 3,
-          min: -3,
-          step: 0.1,
-          name: "X-Axis",
+    c: {
+        meshFilter: {
+            type: "MeshFilter",
+            mesh: me,
+            scene: mainScene,
+        },
+        script: {
+            type: "Script",
+            script: new NewScript(),
+        },
+        transform: {
+            type: "Transform",
+            obj: me,
+        },
+        gui: {
+            type: "GUIcomponent",
+            list: [
+                {
+                    path: [""],
+                    guiType: "slider",
+                    properityName: "x",
+                    target: me.position,
+                    max: 3,
+                    min: -3,
+                    step: 0.1,
+                    name: "X-Axis",
+                }]
         },
         script: {
             type: 'Script',
@@ -175,51 +176,53 @@ const exampleBoxEntity = {
             mass: 10
         }
 
+
+
     }
 };
 
 const water = new Water(65, 4000000, 14000000);
 const waterEntity = {
-  c: {
-    meshFilter: {
-      type: "MeshFilter",
-      mesh: water,
-      scene: mainScene,
-    },
-    transform: {
-      type: "Transform",
-      obj: water,
-    },
-    script: {
-      type: "Script",
-      script: new WaterBehavior(),
-    },
-    gui: {
-      type: "GUIcomponent",
-      list: [
-        {
-          path: ["box", "color", "ammis"],
-          guiType: "slider",
-          properityName: "x",
-          target: mesh.position,
-          max: 3,
-          min: -3,
-          step: 0.1,
-          name: "X-Axis",
+    c: {
+        meshFilter: {
+            type: "MeshFilter",
+            mesh: water,
+            scene: mainScene,
         },
-      ],
+        transform: {
+            type: "Transform",
+            obj: water,
+        },
+        script: {
+            type: "Script",
+            script: new WaterBehavior(),
+        },
+        gui: {
+            type: "GUIcomponent",
+            list: [
+                {
+                    path: ["box", "color", "ammis"],
+                    guiType: "slider",
+                    properityName: "x",
+                    target: mesh.position,
+                    max: 3,
+                    min: -3,
+                    step: 0.1,
+                    name: "X-Axis",
+                },
+            ],
+        },
     },
-  },
 };
 
 const entities = [
-  gameRenderEntity,
-  boxEntity,
-  cameraEntity,
-  postProcessingEntity,
-  skyEntity,
-  exampleBoxEntity,
-  waterEntity,
+    gameRenderEntity,
+    boxEntity,
+    cameraEntity,
+    postProcessingEntity,
+    skyEntity,
+    exampleBoxEntity,
+    waterEntity,
 ];
 
 export { entities };
